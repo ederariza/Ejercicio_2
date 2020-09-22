094f8cb83845aa30a963f1d949e370256510573f
Infromatica II <eder.ariza@udea.edu.co>
2020-09-22 13:10:00 -0500

    Ejercicio 2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
+/*
+Ejercicio 2. Escriba un programa que pida un número N e imprima en pantalla si es par o impar.
+Ej: si se ingresa 5 se debe imprimir:
+5 es impa
+ */
+
 #include <iostream>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+int main(){
+
+    int A, B;
+    cout<< "Ingrese el numero A: ";
+    cin>> A;
+    B = A % 2;
+
+    if(B == 1){
+        cout << A << " es impar" << endl;
+    }
+
+    else{
+            cout << A << " es par" << endl;
+   }
+
     return 0;
 }
